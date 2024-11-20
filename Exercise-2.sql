cb016bf20b0221e20a2e23ad3e25767964480869
Yariaz0801 <designsbyzairay@gmail.com>
2024-11-20 15:21:19 -0500

    Polished Completed Northwind Exercise-2

--- Exercise-2.sql
+++ Exercise-2.sql
@@ -1,64 +1,64 @@
 use northwind;
 
-products
+#1 products
 
-SELECT ProductID, ProductName, UnitPrice
+#2 SELECT ProductID, ProductName, UnitPrice
 -- FROM products;productsproducts
 
-SELECT ProductID, ProductName, UnitPrice
+#3 SELECT ProductID, ProductName, UnitPrice
 -- FROM products
 -- ORDER BY UnitPrice;
 
-SELECT *
+#4 SELECT *
 -- FROM products
 -- WHERE UnitPrice <= 7.50;
 
- SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+#5 SELECT ProductID, ProductName, UnitPrice, UnitsInStock
 -- FROM products
 -- WHERE UnitsInStock >= 100
 -- ORDER BY UnitPrice;
 
-SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+#6 SELECT ProductID, ProductName, UnitPrice, UnitsInStock
 -- FROM products
 -- WHERE UnitsInStock >= 100
 -- ORDER BY UnitPrice desc, ProductName;
 
-SELECT ProductID, ProductName, UnitPrice, UnitsInStock, UnitsOnOrder
+#7 SELECT ProductID, ProductName, UnitPrice, UnitsInStock, UnitsOnOrder
 -- FROM products
 -- WHERE UnitsInStock = 0 AND UnitsOnOrder >= 1
 -- ORDER BY ProductName;
 
-Categories
+#8 Categories
 
-SELECT *
+#9 SELECT *
 -- FROM categories;
 -- WHERE categoryname = "Seafood" ;
 -- The seafood ID is 8
 
-SELECT *
+#10 SELECT *
 -- FROM products
 -- WHERE CategoryID = "8";
 
- SELECT 
+#11 SELECT 
 -- EmployeeID, FirstName, LastName,
 -- concat(FirstName, " ", lastName) AS FullName
 -- FROM employees;
 
-SELECT *
+#12 SELECT *
 -- FROM employees
 -- WHERE title LIKE "%Manager%";
 
-SELECT distinct Title
+#13 SELECT distinct Title
 -- FROM employees;
 
-SELECT EmployeeID, FirstName, LastName, Salary
+#14 SELECT EmployeeID, FirstName, LastName, Salary
 -- FROM employees
 -- WHERE salary BETWEEN 2000 AND 2500;
 
-SELECT *
+#15 SELECT *
 -- FROM suppliers;
 
-SELECT SupplierID, CompanyName
+#16 SELECT SupplierID, CompanyName
 --  FROM suppliers
 --  WHERE CompanyName = "Tokyo Traders";
 
